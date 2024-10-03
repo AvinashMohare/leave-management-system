9c9b3e145c833d2e1527fee6556c319ad1e5797b
Avinash Mohare <avinash@Avinashs-MacBook-Air.local>
2024-10-04 03:15:12 +0530

    Added firebase.js with environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,29 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, prodErrorMap } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC7x4ERHbGllvDspxYpHzSPC0Mue36k_AA",
-//   authDomain: "employeeleavemanagements-68e10.firebaseapp.com",
-//   databaseURL:
-//     "https://employeeleavemanagements-68e10-default-rtdb.firebaseio.com",
-//   projectId: "employeeleavemanagements-68e10",
-//   storageBucket: "employeeleavemanagements-68e10.appspot.com",
-//   messagingSenderId: "1051261609924",
-//   appId: "1:1051261609924:web:cad9c0c6e585e175096691",
-//   measurementId: "G-NT126NF20K",
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBI3CfiSECWoPnpNUvUOVWZ7qZn2N8eeJ8",
-  authDomain: "leave-management-system-138b1.firebaseapp.com",
-  databaseURL: "https://leave-management-system-138b1-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  projectId: "leave-management-system-138b1",
-  storageBucket: "leave-management-system-138b1.appspot.com",
-  messagingSenderId: "1072021421405",
-  appId: "1:1072021421405:web:64f19c14aea3e639e6fcb4"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
